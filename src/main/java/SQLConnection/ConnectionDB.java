c5a95769a7612ac002c8034f5dd2d88f91336958
samekmd <samuelalkmc@gmail.com>
2024-04-18 10:00:32 -0300

    teste de mudança de icone

--- src/main/java/SQLConnection/ConnectionDB.java
+++ src/main/java/SQLConnection/ConnectionDB.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class ConnectionDB {
     public Connection getConnection() {
 
+        //testando
         try {
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/Estoque","root","1234");
         }
