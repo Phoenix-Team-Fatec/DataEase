0f16a31663c855f72220d40d8f9cba7a521ad19e
samekmd <samuelalkmc@gmail.com>
2024-04-24 17:41:57 -0300

    Função esquema do banco de dados

--- src/main/java/SQLConnection/ConnectionDB.java
+++ src/main/java/SQLConnection/ConnectionDB.java
@@ -1,8 +1,8 @@
 package SQLConnection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionDB {
     public Connection getConnection() {
@@ -14,4 +14,51 @@ public class ConnectionDB {
             throw new RuntimeException(excecao);
         }
     }
+
+    public Map<String, String[]> architectureDB() {
+        Map<String, String[]> tablesColumnsMap = new HashMap<>();
+        try {
+            Connection connection = getConnection();
+
+            DatabaseMetaData metaData = connection.getMetaData();
+
+            String databaseName = connection.getCatalog();
+
+            ResultSet tables = metaData.getTables(databaseName,null,null,new String[]{"TABLE"});
+
+            while (tables.next()){
+                String tableName = tables.getString("TABLE_NAME");
+
+                String[] columnsArray;
+
+                ResultSet columns = metaData.getColumns(databaseName,null,tableName,null);
+
+                int columCount = 0;
+                while (columns.next()){
+                    columCount++;
+                }
+
+                columns.beforeFirst();
+
+                columnsArray = new String[columCount];
+
+                int index = 0;
+                while (columns.next()){
+                    String columnName = columns.getString("COLUMN_NAME");
+                    columnsArray[index] = columnName;
+                    index++;
+                }
+                columns.close();
+
+                tablesColumnsMap.put(tableName,columnsArray);
+            }
+            tables.close();
+            connection.close();
+        }catch (SQLException excecao){
+            throw new RuntimeException();
+        }
+        return tablesColumnsMap;
+    }
+
+
 }
