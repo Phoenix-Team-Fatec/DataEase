98d3e2c8c51b085ce8827a7beccf79441eccaa1c
samekmd <samuelalkmc@gmail.com>
2024-04-25 16:40:38 -0300

    Testando código

--- src/main/java/LangChain/LmConnection.java
+++ src/main/java/LangChain/LmConnection.java
@@ -1,26 +1,10 @@
 package LangChain;
 
 import SQLConnection.ConnectionDB;
-import dev.ai4j.openai4j.embedding.EmbeddingModel;
-import dev.langchain4j.data.document.DocumentSplitter;
-import dev.langchain4j.data.embedding.Embedding;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.data.segment.TextSegmentTransformer;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.localai.LocalAiChatModel;
-import dev.langchain4j.store.embedding.EmbeddingStore;
-import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
-import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 
 public class LmConnection  {
 
