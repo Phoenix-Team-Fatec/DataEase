ac4652d5b9c9f54372e88791010cf7bf44b965b0
MatheusAndrade1999 <matheusluiz194@gmail.com>
2024-05-14 19:23:23 -0300

    Sidebar pré-finalizada

--- src/main/java/UserScreen/TelaChat.java
+++ src/main/java/UserScreen/TelaChat.java
@@ -1,3 +1,4 @@
+
 package UserScreen;
 
 import LangChain.LmConnection;
@@ -35,9 +36,16 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
     private JComboBox usersBD = new JComboBox<>();
     private JButton botao_add_bd = new JButton("+");
     private JLabel texto_Adiciona_bd = new JLabel("Novo Banco de dados");
-
     JButton botao_enviar;
     private JComboBox db_users = new JComboBox<>();
+    private JComboBox db_instance = new JComboBox<>();
+    private JLabel texto_db = new JLabel("Banco de Dados:");
+    private JLabel texto_instance = new JLabel("Instancia:");
+    private JLabel texto_users = new JLabel("Usuario:");
+
+    private JButton side_bar  = new JButton("=");
+
+    private JButton exit_side = new JButton("x");
 
     public TelaChat(String nome, String senha){
         ScreenComponents();
@@ -56,11 +64,13 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
 
         //Configurações dos painéis;
         //Painel Chat
-        painel_chat.setSize(850, 600);
-        painel_chat.setVisible(true);
+        painel_chat.setSize(250, 600);
+        painel_chat.setVisible(false);
         painel_chat.setLayout(null);
-        painel_chat.setBackground(new Color(44, 10, 75));
+        painel_chat.setBackground(new Color(44, 10, 200));
         add(painel_chat);
+
+        
 
         //Caixa Resposta
         caixa_resposta = new JLabel();
@@ -68,6 +78,7 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
         caixa_resposta.setBounds(37, 65, 776, 411); // Define a posição (x, y) e o tamanho (largura, altura)
         caixa_resposta.setBorder(new LineBorder(new Color(224, 170, 255)));
         caixa_resposta.setVisible(true);
+        add(caixa_resposta);
 
         //Caixa Entrada
         caixa_entrada = new JTextField();
@@ -76,6 +87,7 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
         caixa_entrada.setForeground(Color.white);
         caixa_entrada.setBorder(new LineBorder(new Color(224, 170, 255)));
         caixa_entrada.setVisible(true);
+        add(caixa_entrada);
 
         //Botao_enviar_prompt
         botao_enviar = new JButton("OK");
@@ -83,24 +95,61 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
         botao_enviar.setBorder(new LineBorder(new Color(224, 170, 255)));
         botao_enviar.setBackground(new Color(157,78,221));
         botao_enviar.addActionListener(this::sendText);
-
-        painel_chat.add(caixa_resposta);
-        painel_chat.add(caixa_entrada);
-        painel_chat.add(botao_enviar);
+        add(botao_enviar);
 
 
-        //Dropbox para seleção do banco de dados do usuário e Botão para adição do mesmo
-        painel_chat.add(usersBD);
-        usersBD.setBounds(37,35,150,20);
 
-        painel_chat.add(texto_Adiciona_bd);
-        texto_Adiciona_bd.setBounds(200,35,150,20);
-        texto_Adiciona_bd.setForeground(Color.white);
 
-        painel_chat.add(botao_add_bd);
+        //Dropdown para seleção do banco de dados do usuário e Botão para adição do mesmo
+
+
+        add(botao_add_bd);
         botao_add_bd.setBounds(340,39,50,15);
         botao_add_bd.addActionListener(this::changeToTelaDB);
 
+        //painel chat
+        painel_chat.add(db_instance);
+        painel_chat.add(db_users);
+        painel_chat.add(texto_db);
+        painel_chat.add(texto_instance);
+        painel_chat.add(texto_users);
+        painel_chat.add(usersBD);
+        usersBD.setBounds(40,80,180,20);
+
+        painel_chat.add(texto_db);
+        texto_db.setBounds(10,60,100,20);
+        texto_db.setForeground(Color.white);
+
+        painel_chat.add(texto_instance);
+        texto_instance.setBounds(10,200,100,20);
+        texto_instance.setForeground(Color.white);
+        db_instance.setBounds(40, 220, 180, 20);
+
+        painel_chat.add(texto_users);
+        texto_users.setBounds(10,340,100,20);
+        texto_users.setForeground(Color.white);
+        db_users.setBounds(40, 360, 180, 20);
+
+        add(side_bar);
+        side_bar.setBounds(20,20,50,20);
+        side_bar.addActionListener(this::aparecer_side_bar);
+
+
+        painel_chat.add(exit_side);
+        exit_side.setBounds(10,10,30,20);
+        exit_side.addActionListener(this:: sair_side_bar);
+
+
+    }
+
+    private void sair_side_bar(ActionEvent actionEvent) {
+        painel_chat.setVisible(false);
+        side_bar.setVisible(true);
+    }
+
+    private void aparecer_side_bar(ActionEvent actionEvent) {
+        painel_chat.setVisible(true);
+        side_bar.setVisible(false);
     }
 
     // Abre a tela de cadastro de DB
@@ -137,4 +186,9 @@ public class  TelaChat extends javax.swing.JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public static void main(String[] args) {
+        new TelaChat("matheus", "");
+    }
 }
+
