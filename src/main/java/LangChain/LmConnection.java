7e7ab49dc171a4b64542d30368aee9b950a65473
TaldoGus <xgustavopereirax@gmail.com>
2024-05-24 08:38:26 -0300

    Update LmConnection.java
    Função ligar e desligar o modelo.

--- src/main/java/LangChain/LmConnection.java
+++ src/main/java/LangChain/LmConnection.java
@@ -3,6 +3,10 @@ package LangChain;
 import SQLConnection.ConnectionDB;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.localai.LocalAiChatModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 
 
@@ -39,6 +43,72 @@ public class LmConnection  {
 
          String languageSql = model.generate(this.getContent() + "\n" + connectionDB.architectureDB()  ); //envia a entrada do usuário e o esquema do banco de dados do usuário para gerar o SQL
          return languageSql; // retorna o SQL
+    }
+//____________________________________________
+
+    //Ligar o modelo
+    public void ligarmodel (String nameModel){
+        String command = "cmd.exe /c lms load " + nameModel;
+        // Criação do ProcessBuilder
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        // Configura o comando a ser executado pelo ProcessBuilder
+        processBuilder.command(command.split(" "));
+
+        try {
+            // Iniciar o processo
+            Process process = processBuilder.start();
+
+            // Capturar a saída do processo
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            // Lê e imprime cada linha da saída do processo
+            while ((line = reader.readLine()) != null) {
+
+            }
+
+            // Esperar o término do processo e obter o código de saída
+            int exitCode = process.waitFor();
+
+
+        } catch (IOException | InterruptedException e) {
+            // Captura e imprime exceções que possam ocorrer durante a execução do comando
+            e.printStackTrace();
+
+        }
+    }
+
+    public void desligarmodel (){
+        String command = "cmd.exe /c lms unload --all " ;
+        // Criação do ProcessBuilder
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        // Configura o comando a ser executado pelo ProcessBuilder
+        processBuilder.command(command.split(" "));
+
+        try {
+            // Iniciar o processo
+            Process process = processBuilder.start();
+
+            // Capturar a saída do processo
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            // Lê e imprime cada linha da saída do processo
+            while ((line = reader.readLine()) != null) {
+
+            }
+
+            // Esperar o término do processo e obter o código de saída
+            int exitCode = process.waitFor();
+
+
+        } catch (IOException | InterruptedException e) {
+            // Captura e imprime exceções que possam ocorrer durante a execução do comando
+            e.printStackTrace();
+
+        }
     }
 
 
